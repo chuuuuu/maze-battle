1aaf27e280743000d79fa51736af55562bfd52da
chuchuching3545 <jim35457005@gmail.com>
2021-07-16 03:53:46 +0800

    client gets the maze after login

--- android/app/src/main/java/com/chuchu/mazebattle/MainActivity.java
+++ android/app/src/main/java/com/chuchu/mazebattle/MainActivity.java
@@ -14,10 +14,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
 import okhttp3.FormBody;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -29,7 +35,7 @@ import okio.ByteString;
 
 public class MainActivity extends AppCompatActivity {
     private GameView gameView;
-
+    private final HashMap<String, List<Cookie>> cookieStore = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,11 +64,24 @@ public class MainActivity extends AppCompatActivity {
         /**建立連線*/
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
+                .cookieJar(new CookieJar() {
+                    @Override
+                    public void saveFromResponse(@NotNull HttpUrl httpUrl, @NotNull List<Cookie> list) {
+                        cookieStore.put(httpUrl.host(), list);
+                    }
+
+                    @NotNull
+                    @Override
+                    public List<Cookie> loadForRequest(@NotNull HttpUrl httpUrl) {
+                        List<Cookie> cookies = cookieStore.get(httpUrl.host());
+                        return cookies != null ? cookies : new ArrayList<Cookie>();
+                    }
+                })
                 .build();
         /**設置傳送需求*/
         Request request = new Request.Builder()
-                .url("http://192.168.1.108:5000/1233444")
-//                .header("Cookie","")//有Cookie需求的話則可用此發送
+                .url("http://192.168.1.108:5000/login")
+                .header("Cookie","")//有Cookie需求的話則可用此發送
 //                .addHeader("","")//如果API有需要header的則可使用此發送
                 .build();
         /**設置回傳*/
@@ -77,7 +96,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 /**取得回傳*/
+
+                System.out.println("login");
+                
+            }
+        });
+
+        /**設置傳送需求*/
+        while (cookieStore.get("192.168.1.108") == null);
+        request = new Request.Builder()
+                .url("http://192.168.1.108:5000/maze_with_session")
+                .header("Cookie","")//有Cookie需求的話則可用此發送
+//                .addHeader("","")//如果API有需要header的則可使用此發送
+                .build();
+        /**設置回傳*/
+        call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                /**如果傳送過程有發生錯誤*/
+                //tvRes.setText(e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                /**取得回傳*/
                 try {
+                    //System.out.println(response.body().string());
                     Grids grids = new Grids(10, 10, new JSONObject(response.body().string()));
 
                     gameView.addElements(grids);
